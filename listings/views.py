3f88e3e4be4d37d506eb90433d011cfd7d9b4193
MikeRossi <michael@sinoc.io>
2019-05-20 15:34:16 -0400

    installing jinga addon

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .models import Listing
+
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()        #fetch all listings, no raw sql queries
+
+    context = {
+        'listings': listings         #passing data into dicitonary
+    }
+
+    return render(request, 'listings/listings.html', context) #passing dictionary into template
 
 def listing(request):
     return render(request, 'listings/listing.html')
